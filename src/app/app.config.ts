cfa73f422320f51cab2e7f6abf34b91e888e6a1b
Florin-Robert Pap <165192808+RobertPap57@users.noreply.github.com>
2025-05-15 23:07:30 +0200

    third commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { firebaseConfig } from './firebase.config';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -8,9 +9,16 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimations(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
-    "projectId":"simple-crm-82042","appId":"1:341035180485:web:c31988e4ebd65a274babac","storageBucket":"simple-crm-82042.firebasestorage.app","apiKey":"AIzaSyAS9YOqlu90zGswlkDOyC-J9xM7XRgIZ1s","authDomain":"simple-crm-82042.firebaseapp.com","messagingSenderId":"341035180485"
 
-  })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()),]
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideAnimations(),
+  provideAnimationsAsync(),
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideAuth(() => getAuth()),
+  provideFirestore(() => getFirestore()),
+  provideDatabase(() => getDatabase()),
+  provideAnimationsAsync(),
+  provideAnimationsAsync(),]
 };
--- src/app/user-detail/user-detail.component.spec.ts
+++ src/app/user-detail/user-detail.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { UserDetailComponent } from './user-detail.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 
 
 
@@ -12,7 +15,10 @@ describe('UserDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserDetailComponent, ],
-      providers: [provideRouter([]),],
+      providers: [provideRouter([]), provideFirebaseApp(() => initializeApp({
+     "projectId":"simple-crm-82042","appId":"1:341035180485:web:c31988e4ebd65a274babac","storageBucket":"simple-crm-82042.firebasestorage.app","apiKey":"AIzaSyAS9YOqlu90zGswlkDOyC-J9xM7XRgIZ1s","authDomain":"simple-crm-82042.firebaseapp.com","messagingSenderId":"341035180485"
+      })),
+      provideFirestore(() => getFirestore()),],
     })
     .compileComponents();
 
